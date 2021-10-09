03f253cd6ffa8c5564b558fb8d8e656d0931b8ff
LitvinovVN <litvinovvn@rambler.ru>
2021-10-09 11:31:46 +0300

    09.10.2021_11:31:46,87

---
+++ Grid3DFragmentMATMOptimization/MatmAlgsTesting/MatmAlgsTesting/run2.sh
@@ -0,0 +1,23 @@
+git pull
+
+cat description.md
+echo ''
+echo '------- Compiling MatmAlgsTesting.cpp into MatmAlgsTesting.o: mpicxx -O3 -c MatmAlgsTesting.cpp ------'
+g++ --std=c++11 -O3 -c MatmAlgsTesting.cpp
+echo '-------- mpicxx -o myapp MatmAlgsTesting.o -----'
+g++ -o myapp MatmAlgsTesting.o
+echo '-------------'
+
+echo '------- Starting myapp in 1 node with 1 cpu per node with maxtime by default (5 minutes) ------'
+echo '------- mpirun -np 1 ./myapp --------'
+mpirun -np 1 ./myapp
+
+echo '------- Task list: mps ---------'
+mps
+
+echo '------- Task list: pult t ---------'
+pult t
+
+echo '------- Status of executed task: mqtest myapp.1 --------'
+mqtest myapp.1
+echo '------- End of run.sh ------'
