f2246ec3bce769e5cca03b44e834f89d75eb6f84
litvinovvn <litvinovvn@rambler.ru>
2021-10-18 08:50:18 +0300

    18.10.2021_ 8:50:18.48

--- ModelingSystemForHCS/src/Grid3DSrc/Utils.h
+++ ModelingSystemForHCS/src/Grid3DSrc/Utils.h
@@ -5,6 +5,8 @@
 
 void app_init(int argc, char* argv[]);
 
+void GetMachineName(char machineName[150]);
+void mpi_init(int argc, char* argv[], int mpi_thread_type, int& provided, int& rank, int& size);
 void printTime();
 
 #endif
