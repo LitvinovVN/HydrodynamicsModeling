7590f1858c9ea2e1aa00acb30d365e0316046020
litvinovvn <LitvinovVN@rambler.ru>
2021-12-26 10:26:15 +0300

    26.12.2021_10:26:14.97

---
+++ ModelingSystemForHCS_ControlModule/ModelingSystem/Shared/Entities/Cluster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelingSystem.Shared.Entities
+{
+    public class Cluster
+    {
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Caption { get; set; } = string.Empty;
+    }
+}
